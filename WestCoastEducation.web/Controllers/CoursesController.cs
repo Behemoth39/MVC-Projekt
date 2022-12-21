2ccd1047f7b664f3c3e65966fe5777b7b5f1d125
Behemoth39 <simme_jonsson@hotmail.com>
2022-12-21 14:06:46 +0100

    added links

--- WestCoastEducation.web/Controllers/CoursesController.cs
+++ WestCoastEducation.web/Controllers/CoursesController.cs
@@ -2,11 +2,17 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace WestCoastEducation.web.Controllers
 {
-    [Route("[controller]")]
+    [Route("courses")]
     public class CoursesController : Controller
     {      
         public IActionResult Index()
         {
+            ViewBag.Courses = new List<string>{
+                "Web Development",
+                "Mechanical",
+                "Technician",
+                "Electrician"
+            };
             return View("Index");
         }
     }

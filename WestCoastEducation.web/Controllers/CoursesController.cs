aa2d28c4cc63d066e9cce2a9cd3672aee67e3a92
Behemoth39 <simme_jonsson@hotmail.com>
2023-01-06 14:35:20 +0100

    Städade ytterligare

--- WestCoastEducation.web/Controllers/CoursesController.cs
+++ WestCoastEducation.web/Controllers/CoursesController.cs
@@ -1,27 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using WestCoastEducation.web.Data;
+using WestCoastEducation.web.Interfaces;
+using WestCoastEducation.web.ViewModels;
 
 namespace WestCoastEducation.web.Controllers;
 
 [Route("courses")]
 public class CoursesController : Controller
 {
-    private readonly WestCoastEducationContext _context;
-    public CoursesController(WestCoastEducationContext context)
+    private readonly IUnitOfWork _unitOfWork;
+
+    public CoursesController(IUnitOfWork unitOfWork)
     {
-        _context = context;
+        _unitOfWork = unitOfWork;
     }
 
     public async Task<IActionResult> Index()
-    {        
-        var courses = await _context.Courses.ToListAsync();
+    {
+        var courses = await _unitOfWork.CourseRepository.ListAllAsync();
         return View("Index", courses);
     }
 
     [Route("course/{courseName}")]
     public IActionResult Details(string courseName)
-    {             
+    {
         return View("course");
     }
 }

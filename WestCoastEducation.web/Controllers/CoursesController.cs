2dbf8e6c05f64b2c2929e1da6fde88b626616f3e
Behemoth39 <simme_jonsson@hotmail.com>
2023-01-06 16:02:47 +0100

    Added user list in regular view

--- WestCoastEducation.web/Controllers/CoursesController.cs
+++ WestCoastEducation.web/Controllers/CoursesController.cs
@@ -17,10 +17,19 @@ public class CoursesController : Controller
     public async Task<IActionResult> Index()
     {
         var courses = await _unitOfWork.CourseRepository.ListAllAsync();
-        return View("Index", courses);
+        var model = courses.Select(c => new CourseListViewModel
+        {
+            CourseId = c.CourseId,
+            CourseName = c.CourseName,
+            CourseNumber = c.CourseNumber,
+            CourseTitle = c.CourseTitle,
+            CourseStart = c.CourseStart,
+            CourseLenght = c.CourseLenght
+        }).ToList();
+        return View("Index", model);
     }
 
-    [Route("course/{courseName}")]
+    [Route("course/{courseName}")] //används ej än
     public IActionResult Details(string courseName)
     {
         return View("course");
